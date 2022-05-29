64c137fcf7d6cc6f94783f07f3f5d7d92d422049
Kewon <48837736+kewon96@users.noreply.github.com>
2022-05-29 17:52:19 +0900

    Initial commit

---
+++ src/main/java/com/smv/springmvc2_vue/SpringMvc2VueApplication.java
@@ -0,0 +1,13 @@
+package com.smv.springmvc2_vue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvc2VueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvc2VueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/smv/springmvc2_vue/SpringMvc2VueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smv.springmvc2_vue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvc2VueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
